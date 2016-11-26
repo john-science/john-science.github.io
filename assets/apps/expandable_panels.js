37bb99b507ef0441275f0f2a541847867a840c8c
John Stilley <thejollysin@gmail.com>
2016-11-26 13:16:05 -0800

    #39 removing jquery from collapsable panels

--- assets/apps/expandable_panels.js
+++ assets/apps/expandable_panels.js
@@ -1,88 +1,67 @@
-(function($) {
-	$(document).ready(function() {
-		// Init variables
-		var panelspeed = 500; //panel animate speed in milliseconds
-		var totalpanels = document.getElementById("number_of_panels").value;
-		var defaultopenpanel = 0; //leave 0 for no panel open
-		var accordian = true; //set panels to behave like an accordian, with one panel only ever open at once
-		var panelheight = new Array();
-		var currentpanel = defaultopenpanel;
-		var iconheight = parseInt($('.icon-close-open').css('height'));
-		var highlightopen = true;
+// Init variables
+var totalpanels = document.getElementById("number_of_panels").value;
+var defaultopenpanel = 0; //leave 0 for no panel open
+var accordian = true; //set panels to behave like an accordian, with one panel only ever open at once
+var panelheight = new Array();
+var currentpanel = defaultopenpanel;
+var iconheight = parseInt(window.getComputedStyle(document.getElementsByClassName('icon-close-open')[0]).getPropertyValue('height'));
+var highlightopen = true;
 
-		// Initialise collapsible panels
-		function panelinit() {
-			for (var i = 1; i <= totalpanels; i++) {
-				panelheight[i] = parseInt($('#cp-' + i).find('.expandable-panel-content').css('height'));
-				$('#cp-' + i).find('.expandable-panel-content').css('margin-top', -panelheight[i]);
-				if (defaultopenpanel == i) {
-					$('#cp-' + i).find('.icon-close-open').css('background-position', '0px -' + iconheight + 'px');
-					$('#cp-' + i).find('.expandable-panel-content').css('margin-top', 0);
-				}
-			}
+// Initialise collapsible panels
+function panelinit() {
+  for (var i = 1; i <= totalpanels; i++) {
+    var thisPanel = document.getElementById("cp-" + i + "-expandable-panel-content");
+    panelheight[i] = parseInt(window.getComputedStyle(thisPanel).height);
+    thisPanel.setAttribute("style", "margin-top: " + -panelheight[i] + ";");
+    if (defaultopenpanel == i) {
+      document.getElementById('cp-' + i + '-icon').setAttribute("style", "background-position: 0px -" + iconheight + "px;");
+      thisPanel.setAttribute("style", "margin-top: 0;");
+    }
+  }
 
-			// init user count value for each panel
-			for (var k=1; k <= totalpanels; k++) {
-					document.getElementById("cp-" + k.toString() + "-panel-userCount").value = 0;
-			}
-		}
+  // init user count value for each panel
+  for (var k = 1; k <= totalpanels; k++) {
+    document.getElementById("cp-" + k.toString() + "-panel-userCount").value = 0;
+    document.getElementById("cp-" + k + "-expandable-panel-heading").addEventListener("click", panelClick);
+  }
+}
 
-		// on-click
-		$('.expandable-panel-heading').click(function() {
-			var obj = $(this).next();
-			var objid = parseInt($(this).parent().attr('ID').substr(3, 2));
-			currentpanel = objid;
-			if (accordian == true) {
-				resetpanels();
-			}
+// on-click
+function panelClick() {
+  var objid = this.id.split('-')[1];
+  currentpanel = objid;
+  if (accordian == true) {
+    resetpanels();
+  }
 
-			if (parseInt(obj.css('margin-top')) <= (panelheight[objid] * -1)) {
-				obj.clearQueue();
-				obj.stop();
-				obj.prev().find('.icon-close-open').css('background-position', '0px -' + iconheight + 'px');
-				obj.animate({
-					'margin-top': 0
-				}, panelspeed);
-				if (highlightopen == true) {
-					$('#cp-' + currentpanel + ' .expandable-panel-heading').addClass('header-active');
-				}
-			} else {
-				obj.clearQueue();
-				obj.stop();
-				obj.prev().find('.icon-close-open').css('background-position', '0px 0px');
-				obj.animate({
-					'margin-top': (panelheight[objid] * -1)
-				}, panelspeed);
-				if (highlightopen == true) {
-					$('#cp-' + currentpanel + ' .expandable-panel-heading').removeClass('header-active');
-				}
-			}
-		});
+  var contentPanel = document.getElementById('cp-' + objid + '-expandable-panel-content');
+  if (parseInt(window.getComputedStyle(contentPanel).getPropertyValue('margin-top')) <= (panelheight[objid] * -1)) {
+    document.getElementById('cp-' + objid + '-icon').setAttribute("style", "background-position: 0px -" + iconheight + "px;");
+    contentPanel.setAttribute("style", "margin-top: 0px;")
+    if (highlightopen == true) {
+      document.getElementById('cp-' + currentpanel + '-expandable-panel-heading').classList.add("header-active");
+    }
+  } else {
+    document.getElementById('cp-' + objid + '-icon').setAttribute("style", "background-position: 0px 0px;");
+    contentPanel.setAttribute("style", "margin-top: -" + panelheight[objid] + "px;")
+    if (highlightopen == true) {
+      document.getElementById('cp-' + currentpanel + '-expandable-panel-heading').classList.remove("header-active");
+    }
+  }
+}
 
-		// Reset
-		function resetpanels() {
-			for (var i = 1; i <= totalpanels; i++) {
-				if (currentpanel != i) {
-					$('#cp-' + i).find('.icon-close-open').css('background-position', '0px 0px');
-					$('#cp-' + i).find('.expandable-panel-content').animate({
-						'margin-top': -panelheight[i]
-					}, panelspeed);
-					if (highlightopen == true) {
-						$('#cp-' + i + ' .expandable-panel-heading').removeClass('header-active');
-					}
-				}
-			}
-		}
+// Reset
+function resetpanels() {
+  for (var i = 1; i <= totalpanels; i++) {
+    if (currentpanel != i) {
+      document.getElementById('cp-' + i + '-icon').setAttribute("style", "background-position: 0px 0px;");
+      document.getElementById('cp-' + i + '-expandable-panel-content').setAttribute("style", "margin-top: -" + panelheight[i] + "px;")
+      if (highlightopen == true) {
+        document.getElementById('cp-' + i + '-expandable-panel-heading').classList.remove("header-active");
+      }
+    }
+  }
+}
 
-		// Uncomment these lines if the panels are not fixed width and need to resize
-		$( window ).resize(function() {
-			 panelinit();
-		 });
-
-		// on load
-		$(window).load(function() {
-			panelinit();
-		});
-
-	}); // end ready
-})(jQuery);
+window.onresize = panelinit;
+window.onload = panelinit;
