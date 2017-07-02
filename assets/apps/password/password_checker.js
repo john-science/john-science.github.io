d4aae660a265133f732940169edb2e14e20e3fcb
theJollySin <thejollysin@gmail.com>
2017-07-02 00:21:30 -0700

    #69 adding data to known passwords

--- assets/apps/password/password_checker.js
+++ assets/apps/password/password_checker.js
@@ -194,7 +194,7 @@ var full_test = function() {
   stat_list.innerHTML += "<li>Length: " + len + "</li>";
 
   // run all tests and update any results as you find them
-  var results = [test_complexity(comp), test_is_password(str, len), test_length(len), test_int(str), test_date(str), test_repeat(str, len), test_in_array(TOP1000, str, 1, "In the top 1000 most common passwords."), test_in_array(ENG1000, str, 2, "In the English dictionary."), test_in_array(IMMATURE, str, 1, "Immature people are predictable."), test_in_array(KNOWN_DEFAULTS, str, 1, "This is a commonly known default password.")];
+  var results = [test_complexity(comp), test_is_password(str, len), test_length(len), test_int(str), test_date(str), test_repeat(str, len), test_in_array(TOP10000, str, 1, "A common password."), test_in_array(ENG4000, str, 1, "In the English dictionary."), test_in_array(IMMATURE, str, 1, "Immature people are predictable."), test_in_array(KNOWN_DEFAULTS, str, 1, "This is a commonly known default password.")];
   for (var i = 0; i < results.length; i++) {
     var res = results[i];
     if (res) {
