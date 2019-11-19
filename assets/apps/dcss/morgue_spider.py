2b180bfed730f5a860e29b4c25aac6a7a26703a5
John Stilley <1831479+theJollySin@users.noreply.github.com>
2019-11-19 15:26:47 -0500

    Draft 2 now writes out to file more often

--- assets/apps/dcss/morgue_spider.py
+++ assets/apps/dcss/morgue_spider.py
@@ -25,47 +25,29 @@ PAGES = ['https://crawl.kelbi.org/scoring/highscores.html',
 
 
 def main():
-    all_urls = spider(set(PAGES), set(PAGES), int(SEARCH_DEPTH), float(WAIT_SECONDS))
-    morgues = find_morgues(all_urls)
-    write_morgue_urls_to_file(morgues, OUT_NAME)
+    all_urls = morgue_spider(set(PAGES), set(PAGES), OUT_NAME, int(SEARCH_DEPTH), float(WAIT_SECONDS))
+    print('Spidered {0} URLs'.format(len(all_urls)))
 
 
-def write_morgue_urls_to_file(urls, out_name='morgue_urls'):
-    """
-    """
-    # if we have run this script before, we will already have files saved with morgue addresses
-    known_morgues = set()
-    old_morgue_files = glob(out_name + '*')
-    for old_file in old_morgue_files:
-        known_morgues.update([f.strip() for f in open(old_file, 'r').readfiles()])
-
-    # write all the new and unique morgues we have found to a text file
-    with open('{0}_{1}.txt'.format(out_name, datetime.now().strftime('%Y%m%d_%H%M%S')), 'w') as f:
-        for url in sorted(urls):
-            if url not in known_morgues:
-                f.write(url)
-                f.write('\n')
-
-
-def find_morgues(urls):
-    """
-    """
-    return [u for u in urls if u.split('/')[-1].startswith('morgue') and u.endswith('.txt')]
-
-
-def spider(all_urls, new_urls, depth=5, wait_seconds=60.0):
+def morgue_spider(all_urls, new_urls, out_name='morgue_urls', depth=5, wait_seconds=60.0):
     """
     """
     if depth <= 0:
         return all_urls
 
     newer_urls = set()
+    start = datetime.now().timestamp()
     for url in new_urls:
         if url.endswith('.html') and looks_crawl_related(url):
             newer_urls.update(find_links_in_file(url))
             sleep(wait_seconds + 0.5 * wait_seconds * random())
+            if datetime.now().timestamp() - start > 30 * 60 * 60:
+                write_morgue_urls_to_file(newer_urls - all_urls, out_name)
+                start = datetime.now().timestamp()
 
-    return spider(all_urls.union(newer_urls), newer_urls - all_urls, depth - 1)
+    write_morgue_urls_to_file(newer_urls - all_urls, out_name)
+
+    return morgue_spider(all_urls.union(newer_urls), newer_urls - all_urls, out_name, depth - 1)
 
 
 def looks_crawl_related(url):
@@ -76,8 +58,8 @@ def looks_crawl_related(url):
     for term in crawl_terms:
         if term in url:
             return True
-return
-     False
+
+    return False
 
 
 def find_links_in_file(url):
@@ -88,6 +70,37 @@ def find_links_in_file(url):
     soup = BeautifulSoup(html, features="html.parser")
     all_links = soup.findAll('a')
     return set([a['href'].strip() for a in all_links])
+
+
+def write_morgue_urls_to_file(all_urls, out_name='morgue_urls'):
+    """
+    """
+    urls = find_morgues(all_urls)
+
+    # if we have run this script before, we will already have files saved with morgue addresses
+    known_morgues = set()
+    old_morgue_files = glob(out_name + '*')
+    for old_file in old_morgue_files:
+        known_morgues.update([f.strip() for f in open(old_file, 'r').readlines()])
+
+    urls = [u for u in urls if u not in known_morgues]
+
+    if not len(urls):
+        print("Found no new morgues.")
+    else:
+        print("Found {0} new morgues".format(len(urls)))
+
+    # write all the new and unique morgues we have found to a text file
+    with open('{0}_{1}.txt'.format(out_name, datetime.now().strftime('%Y%m%d_%H%M%S')), 'a+') as f:
+        for url in sorted(urls):
+            f.write(url)
+            f.write('\n')
+
+
+def find_morgues(urls):
+    """
+    """
+    return [u for u in urls if u.split('/')[-1].startswith('morgue') and u.endswith('.txt')]
 
 
 if __name__ == '__main__':
