01b601d8b7a2d1e292b7d728288cb783119f834d
John Stilley <1831479+theJollySin@users.noreply.github.com>
2019-11-25 14:35:59 -0500

    Trial script, got it working on a few morgue files

--- assets/apps/dcss/morgue_parser.py
+++ assets/apps/dcss/morgue_parser.py
@@ -1,48 +1,92 @@
+"""
+"""
+import requests
+from sys import argv
+
 
 def main():
-    # TODO:
-    pass
+    # TODO: Normalize species, backgrounds, and Gods on output
+    # TODO: function to read text from a file and return stats
+    # TODO: function to grab text from a link and return the stats
+    # TODO: function to loop through links (or files) and build a winners file
+    for url in argv[1:]:
+        txt = read_url(url)
+        try:
+            print(parse_one_morgue(txt))
+        except ParserError as e:
+            print('ParserError', e, url)
+        except Loser as e:
+            print('Loser', url)
 
 
-# TODO: Add in ability to parse these awful HTML versions of morgues: 
-#       http://lazy-life.ddo.jp:8080/morgue/anselm/morgue-anselm-20160315-131124.txt
+def read_url(url):
+    """
+    """
+    r = requests.get(url)
+    return r.content.decode("utf-8")
+
+
 def parse_one_morgue(txt):
+    txt = strip_html(txt)
+
     lines = txt.split('\n')[:20]
     if len(lines) < 13:
-        raise Exception('Invalid file, not long enough')
+        raise ParserError('Invalid file, not long enough')
     elif not lines[0].startswith(' Dungeon Crawl Stone Soup version '):
-        raise Exception('Invalid file, starting line not found')
+        raise ParserError('Invalid file, starting line not found')
     elif "Escaped with the Orb" not in txt:
-        raise Exception('This is not a winning run.')
+        raise Loser('This is not a winning run.')
 
-    version = lines[0].split(' version ')[1].split('-')[0].split()[0]
+    version = lines[0].split(' version ')[1].split('-')[0].split()[0].split('.')
+    version = '.'.join([version[0], version[1]])
 
+    god = ''
     num_runes = -1
     the_line = ''
     for line in lines[1:]:
-        if not len(line.strip()):
-            continue
-        elif line.strip().startswith('... and '):
-            num_runes = int(line.split('... and ')[1].split(' runes')[0])
-        elif ('the' in line) and ('(' in line) and (')' in line) and ('Turns:' in line) and ('Time:' in line):
-            the_line = line
-            break
 
     if not len(the_line) or num_runes < 0:
-        raise Exception('Error parsing file')
+        raise ParserError('Error parsing file')
 
     species = background = ''
     build = the_line.split('(')[1].split(')')[0].lower()
-    for spec in SPECIES:
-        if build.startswith(spec):
-            species = spec
-            background = build.split(spec).strip()
-            break
+    if ' ' not in build and len(build) == 4:
+        # cover the case where builds are written as (OpEE)
+        species = build[:2]
+        background = build[2:]
+    else:
+        # cover the case where builds are written as (Octopode Earth Elementalist)
+        for spec in SPECIES:
+            if build.startswith(spec):
+                species = spec
+                background = build.split(spec)[1].strip()
+                break
 
     if not len(species) or not len(background):
-        raise Exception('Error parsing build info')
+        raise ParserError('Error parsing build info')
 
-    return species, background, num_runes, version
+    return species, background, god, num_runes, version
+
+
+def strip_html(txt):
+    """ strip HTML from a non-raw dump, if any exists
+    """
+    if "<!DOCTYPE html>" in txt or "<html>" in txt:
+        i = txt.find(' Dungeon Crawl Stone Soup version ')
+        if i < 21:
+            return ''
+        else:
+            return txt[i:].split('</pre>')[0]
+    else:
+        return txt
+
+
+class Loser(Exception):
+    pass
+
+
+class ParserError(Exception):
+    pass
 
 
 SPECIES = {'barachian', 'black draconian', 'centaur', 'deep dwarf', 'deep elf', 'demigod', 'demonspawn', 'djinni',
@@ -51,6 +95,9 @@ SPECIES = {'barachian', 'black draconian', 'centaur', 'deep dwarf', 'deep elf', 
            'mottled draconian', 'mountain dwarf', 'mummy', 'naga', 'octopode', 'ogre', 'pale draconian',
            'purple draconian', 'red draconian', 'sludge elf', 'spriggan', 'tengu', 'troll', 'vampire',
            'vine stalker', 'white draconian', 'yellow draconian'}
+
+GODS = {'muna': 'Sif Muna', 'madash': 'Fedhas Madash', 'sagoz': 'Gozag Ym Sagoz', 'xobeh': 'Nemelex Xobeh',
+        'stormbringer': 'Qazlal Stormbringer', 'council': 'The Wu Jian Council', 'one': 'The Shining One'}
 
 
 if __name__ == '__main__':
